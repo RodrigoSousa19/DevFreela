c889533718112d4b0826580eed8fb984fe483dfc
Rodrigo Santos <rodrigocbs914@gmail.com>
2025-01-29 22:43:03 -0300

    feat: finalizando modulo de CQRS, adicionando testes.

--- DevFreela.Application/Commands/InsertProject/InsertProjectHandler.cs
+++ DevFreela.Application/Commands/InsertProject/InsertProjectHandler.cs
@@ -1,4 +1,5 @@
-﻿using DevFreela.Core.Entities;
+﻿using DevFreela.Application.Notification.ProjectCreated;
+using DevFreela.Core.Entities;
 using DevFreela.Infrastructure.Persistence;
 using MediatR;
 
@@ -7,9 +8,11 @@ namespace DevFreela.Application.Commands.InsertProject
     internal class InsertProjectHandler : IRequestHandler<InsertProjectCommand, ResultViewModel<Project>>
     {
         private readonly DevFreelaDbContext _context;
-        public InsertProjectHandler(DevFreelaDbContext context)
+        private readonly IMediator _mediator;
+        public InsertProjectHandler(DevFreelaDbContext context, IMediator mediator)
         {
             _context = context;
+            _mediator = mediator;
         }
 
         public async Task<ResultViewModel<Project>> Handle(InsertProjectCommand request, CancellationToken cancellationToken)
@@ -18,6 +21,9 @@ namespace DevFreela.Application.Commands.InsertProject
 
             await _context.Projects.AddAsync(project);
             await _context.SaveChangesAsync();
+
+            var projectCreated = new ProjectCreatedNotification(project.Id, project.Title, project.TotalCost);
+            await _mediator.Publish(projectCreated);
 
             return ResultViewModel<Project>.Success(project);
         }
---
+++ DevFreela.UnitTests/Core/ProjectTests.cs
@@ -0,0 +1,45 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.UnitTests.Core
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void ProjectIsCreated_Start_Success()
+        {
+            //arrange
+            var project = new Project("Projeto A", "Descrição A", 1, 2, 1234.34M);
+
+            //act
+            project.Start();
+
+            //Assert
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+
+            Assert.True(project.Status == ProjectStatusEnum.InProgress);
+            Assert.False(project.StartedAt is null);
+        }
+
+        [Fact]
+        public void ProjectIsInInvalidState_Start_ThrowsException()
+        {
+            //arrange
+            var project = new Project("Projeto A", "Descrição A", 1, 2, 1234.34M);
+            project.Start();
+
+            //act + assert
+            Action? start = project.Start;
+
+            var exception = Assert.Throws<InvalidOperationException>(start);
+
+            Assert.Equal(Project.INVALID_STATE_MESSAGE, exception.Message);
+        }
+    }
+}
